8d81521ba7c226c67fd00e12770ee799546b5348
Peter Širka <petersirka@gmail.com>
2023-04-12 20:18:25 +0200

    Fixed settings + improved compiler.

--- public/js/uibuilder.js
+++ public/js/uibuilder.js
@@ -970,14 +970,14 @@
 
 					if (arr) {
 						for (var m of arr) {
-							app.inputs.push({ id: instance.id + '_' + m.id, ref: m.id, name: name + ': ' + m.name, component: name, input: m.name, icon: instance.component.icon, color: instance.component.color, note: m.note, schema: m.schema });
+							app.inputs.push({ id: instance.id + '_' + m.id, ref: m.id, name: name + ': ' + m.name, componentid: instance.component.id, component: name, input: m.name, icon: instance.component.icon, color: instance.component.color, note: m.note, schema: m.schema });
 						}
 					}
 
 					arr = instance.component.outputs;
 					if (arr && arr.length) {
 						for (var m of arr) {
-							app.outputs.push({ id: instance.id + '_' + m.id, ref: m.id, name: name + ': ' + m.name, component: name, output: m.name, icon: instance.component.icon, color: instance.component.color, note: m.note, schema: m.schema });
+							app.outputs.push({ id: instance.id + '_' + m.id, ref: m.id, name: name + ': ' + m.name, componentid: instance.component.id, component: name, output: m.name, icon: instance.component.icon, color: instance.component.color, note: m.note, schema: m.schema });
 						}
 					}
 				}
